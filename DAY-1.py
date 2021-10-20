b00bc636749928a7b3bb6b2d5d759c8551c28529
SnehaShet22 <92879393+SnehaShet22@users.noreply.github.com>
2021-10-21 00:09:00 +0530

    Add files via upload

---
+++ DAY-1.py
@@ -0,0 +1,127 @@
+'''
+void main()
+{
+
+    int a , char b, float c
+    a=2
+    b="ughj'
+    c=2.5
+}
+
+'''
+'''
+a=5
+b=5.5
+c='char'
+print(a)
+print(b)
+print(c)
+
+print('hello world')
+'''
+'''
+#variable declaration 
+
+myVariableName = 5 
+
+MyVariableName = 6
+
+my_varable_name = 7 
+
+myVariableName = "hgh"
+
+print(myVariableName)
+print(type(myVariableName))
+print(my_varable_name) 
+
+print(type(MyVariableName))
+
+print(type(my_varable_name)) 
+
+''' 
+
+
+'''
+a = 5
+b = 5.5
+c='char'
+d= "15585556556"
+print(d)
+
+print(int(d))
+print(float(d))
+
+int (c)
+''' 
+
+
+'''
+a = 5 
+b = 10 
+
+print(a + b ) 
+print(a - b)
+print(a * b ) 
+print(a / b)     #quotient   0.5
+print(a % b )    #remainder 
+print(a ** b)    #a raised to b
+print(a // b )   # integer value of division   0 
+'''
+'''
+      0   #quotient
+     ____
+  10) 5
+   - 0
+   _____
+     5      #remainder
+
+''' 
+x = 5 
+x+= 1 # x = x + 1 
+x -= 1 # x = x - 1 
+x *= 1 # x = x * 1
+x /= 1 # x = x / 1 
+
+'''
+a= 5 
+b = 7 
+
+if a != b:
+    print("not equal")
+else:
+    print('a is greater')
+'''
+'''
+a= [1,2,3,5] 
+b = [1,2,3,5]
+if 7 in b:              
+    print("yes")
+else:
+    print("no")
+'''
+
+          #  0        1        2 
+thisList= ["apple",'cherry','pear']
+          #   -3      -2       -1
+print(thisList) 
+
+print(thisList[1])
+print(thisList[-1])
+'''
+thisList.append('banana')
+thisList.append(['mango','tangerine']) #['apple', 'cherry', 'pear', 'banana', ['mango', 'tangerine']]
+thisList.extend(['mango','tangerine'])#['apple', 'cherry', 'pear', 'banana', ['mango', 'tangerine'], 'mango', 'tangerine']
+print(thisList)
+'''
+'''
+a = len(thisList)
+print(a) 
+
+print(type(thisList))
+
+a = (1,2,3,4,5, 5 , 5 , 5.5 ) 
+ab= list(a) 
+print(ab) 
+'''
+
+
